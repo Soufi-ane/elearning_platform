f4bf10980e2677c73fb33c129c739d3ef1df400b
Soufiane <jabersoufiane605@gmail.com>
2026-03-30 18:22:00 +0100

    initial commit -- create spring boot project

---
+++ elearn-core/src/main/java/com/elearn/api/ElearnCoreApplication.java
@@ -0,0 +1,13 @@
+package com.elearn.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearnCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElearnCoreApplication.class, args);
+	}
+
+}
