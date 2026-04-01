2f6badeeb55761802bb3b06dbca569435f9aa867
Soufiane <jabersoufiane605@gmail.com>
2026-04-01 12:46:18 +0100

    add module & element mapping

--- elearn-core/src/main/java/com/elearn/api/entity/Element.java
+++ elearn-core/src/main/java/com/elearn/api/entity/Element.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Data;
@@ -18,5 +20,9 @@ public class Element {
   private String id;
 
   private String name;
+
+  @ManyToOne()
+  @JoinColumn(name = "module_id")
+  private Module module;
 }
 
--- elearn-core/src/main/java/com/elearn/api/entity/Module.java
+++ elearn-core/src/main/java/com/elearn/api/entity/Module.java
@@ -1,11 +1,14 @@
 package com.elearn.api.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Data;
@@ -25,5 +28,8 @@ public class Module {
   @ManyToOne()
   @JoinColumn(name = "department_id")
   private Department department;
+
+  @OneToMany(mappedBy = "module")
+  private List<Element> elements;
 }
 
