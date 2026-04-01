947648fd70a9f112cb2b1c314a34ec0b7b486f87
Soufiane <jabersoufiane605@gmail.com>
2026-04-01 15:26:49 +0100

    add user controller

---
+++ elearn-core/src/main/java/com/elearn/api/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.elearn.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.elearn.api.service.UserService;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+  private final UserService userService;
+
+  @Autowired
+  public UserController(UserService userService){
+    this.userService = userService;
+  }
+}
