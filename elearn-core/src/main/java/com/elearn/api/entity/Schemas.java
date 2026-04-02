cba4ce82c84918ed3ecb03d81a67ab895107baa9
Soufiane <jabersoufiane605@gmail.com>
2026-04-02 16:24:04 +0100

    add schemas

---
+++ elearn-core/src/main/java/com/elearn/api/entity/Schemas.java
@@ -0,0 +1,33 @@
+package com.elearn.api.entity;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class Schemas {
+
+  public record DepartmentBaseResponse(String id, String name){}
+
+  public record RegisterRequest(
+    String firstName, String lastName, 
+    String username, String email, String password, 
+    @JsonFormat(pattern = "dd-MM-yyyy") LocalDate dateOfBirth,
+    Role role, String departmentId
+  ){}
+
+  public record UserBaseResponse(
+    String firstName, String lastName, 
+    String username, String email,
+    LocalDate dateOfBirth, Role role,
+    String departmentId
+  ){
+    public UserBaseResponse(User user){
+      this(
+        user.getFirstName(), user.getLastName(), user.getUsername(),
+        user.getEmail(), user.getDateOfBirth(), user.getRole(),
+        user.getDepartment().getId()
+      );
+    }
+  }
+
+}
