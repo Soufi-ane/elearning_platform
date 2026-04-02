2138db2d7125ac72ad79d3c48088284e2e8cdd5e
Soufiane <jabersoufiane605@gmail.com>
2026-04-02 16:24:38 +0100

    find users by email or username

--- elearn-core/src/main/java/com/elearn/api/repository/UserRepository.java
+++ elearn-core/src/main/java/com/elearn/api/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package com.elearn.api.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.elearn.api.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> { }
+public interface UserRepository extends JpaRepository<User, String> {
+
+  public Optional<User> findByUsernameOrEmail(String username, String email);
+  
+}
