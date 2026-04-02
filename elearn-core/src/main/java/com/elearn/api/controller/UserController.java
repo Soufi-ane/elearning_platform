9effa88f3e14d6cad9659566ef9f597dd048b682
Soufiane <jabersoufiane605@gmail.com>
2026-04-02 16:26:25 +0100

    add register mapping

--- elearn-core/src/main/java/com/elearn/api/controller/UserController.java
+++ elearn-core/src/main/java/com/elearn/api/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.elearn.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.elearn.api.entity.Schemas.RegisterRequest;
+import com.elearn.api.entity.Schemas.UserBaseResponse;
 import com.elearn.api.service.UserService;
 
 @RestController
@@ -14,5 +18,10 @@ public class UserController {
   @Autowired
   public UserController(UserService userService){
     this.userService = userService;
+  }
+
+  @PostMapping("/register")
+  public UserBaseResponse register(@RequestBody RegisterRequest request){
+    return userService.register(request);
   }
 }
