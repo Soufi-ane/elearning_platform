3fd064fdc89736748bd9a52ff784438865dfb6b5
Soufiane <jabersoufiane605@gmail.com>
2026-04-06 15:07:22 +0100

    make register authenticated (only admins can create acounts)

--- elearn-core/src/main/java/com/elearn/api/config/SecurityConfig.java
+++ elearn-core/src/main/java/com/elearn/api/config/SecurityConfig.java
@@ -7,13 +7,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
+  private final JwtAuthFilter jwtAuthFilter;
 
+  public SecurityConfig(JwtAuthFilter jwtAuthFilter){
+    this.jwtAuthFilter = jwtAuthFilter;
+  }
+  
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
     http
@@ -21,11 +27,13 @@ public class SecurityConfig {
       .csrf(csrf -> csrf.disable())
       .authorizeHttpRequests(auth -> auth
           .requestMatchers("/api/v1/departments").permitAll()
-          .requestMatchers("/api/v1/users/register").permitAll()
           .requestMatchers("/api/v1/users/login").permitAll()
+          .requestMatchers("/api/v1/users/register").hasRole("ADMIN")
           .anyRequest().authenticated()
-          )
-      .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        )
+      .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+      .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+    
     return http.build();
   }
 
