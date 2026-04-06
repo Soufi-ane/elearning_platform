32c60cd402637a0c360eb8144294976e7c094b9d
Soufiane <jabersoufiane605@gmail.com>
2026-04-06 15:08:19 +0100

    jwtAuth

--- elearn-core/src/main/java/com/elearn/api/config/ApplicationConfig.java
+++ elearn-core/src/main/java/com/elearn/api/config/ApplicationConfig.java
@@ -2,15 +2,30 @@ package com.elearn.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import com.elearn.api.repository.UserRepository;
 
 @Configuration
 public class ApplicationConfig {
 
+  private final UserRepository userRepository;
+
+  public ApplicationConfig(UserRepository userRepository){
+    this. userRepository = userRepository;
+  }
+
   @Bean 
   public PasswordEncoder passwordEncoder(){
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService(){
+    return identifier -> userRepository.findByEmail(identifier)
+      .orElseThrow(()-> new UsernameNotFoundException("User not found"));
   }
 
 }
---
+++ elearn-core/src/main/java/com/elearn/api/config/JwtAuthFilter.java
@@ -0,0 +1,65 @@
+package com.elearn.api.config;
+
+import java.io.IOException;
+import java.util.List;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import com.elearn.api.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+  private JwtService jwtService;
+  private UserDetailsService userDetailsService;
+
+  public JwtAuthFilter(JwtService jwtService,UserDetailsService userDetailsService){
+    this.jwtService = jwtService;
+    this.userDetailsService = userDetailsService;
+  }
+
+  @Override
+  protected void doFilterInternal(
+    HttpServletRequest request, HttpServletResponse response, FilterChain filterChain
+  ) throws ServletException, IOException {
+    final String authHeader = request.getHeader("Authorization");
+
+    if(authHeader == null || !authHeader.startsWith("Bearer ")){
+      filterChain.doFilter(request, response);
+      return;
+    }
+    System.out.println("=========Authorization=" + authHeader);
+
+    String jwt = authHeader.substring(7);
+    String username = jwtService.extractUsername(jwt);
+
+    System.out.println("=========Username=" + username);
+
+    if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+      UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+      if(jwtService.isTokenValid(jwt, userDetails)){
+        String role = (String) jwtService.extractAllClaims(jwt).get("role");
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + role);
+
+        UsernamePasswordAuthenticationToken authToken =
+          new UsernamePasswordAuthenticationToken(userDetails, null,List.of(authority));
+
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+      }
+    }
+    filterChain.doFilter(request, response);
+  }
+
+}
+
