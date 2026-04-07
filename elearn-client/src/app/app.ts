e0baf76ae0ca16f19d91d65dc2bdcbfd277b25d5
Soufiane <jabersoufiane605@gmail.com>
2026-04-07 18:45:48 +0100

    homepage & login

--- elearn-client/src/app/app.ts
+++ elearn-client/src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import {Footer} from "./components/footer/footer";
+import {Header} from "./components/header/header";
+import {Home} from "./home/home";
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Footer,Header,Home],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('elearn-client');
