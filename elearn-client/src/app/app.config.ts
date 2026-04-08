27e8c953a951cb2511dceb0c4f910ceb106b1d7a
Soufiane <jabersoufiane605@gmail.com>
2026-04-08 21:46:24 +0100

    disable SSR

--- elearn-client/src/app/app.config.ts
+++ elearn-client/src/app/app.config.ts
@@ -1,15 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { provideTanStackQuery, QueryClient } from '@tanstack/angular-query-experimental';
+import { LoginService } from './components/login/login.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideTanStackQuery(new QueryClient()),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideAppInitializer(() => {
+      const loginService = inject(LoginService);
+      return loginService.checkAuth();
+    })
   ]
 };
