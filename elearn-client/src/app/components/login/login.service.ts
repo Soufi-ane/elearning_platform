202b8c029bcc8892c1c361566853ab808ac94a80
Soufiane <jabersoufiane605@gmail.com>
2026-04-08 11:50:09 +0100

    request server for login

---
+++ elearn-client/src/app/components/login/login.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable, signal } from "@angular/core";
+import { Observable } from "rxjs";
+
+type LoginRequest = {
+  usernameOrEmail : string,
+  password : string
+}
+
+type LoginResponse = {
+  user : User,
+  token : string
+}
+
+type User = {
+  firstName : string,
+  lastName : string,
+  username : string,
+  email : string,
+  dateOfBirth : Date,
+  role : Role,
+  departmentId : string
+}
+
+enum Role {
+  STUDENT,
+  TEACHER,
+  ADMIN
+}
+
+@Injectable({providedIn : "root"})
+export class LoginService {
+  private http = inject(HttpClient);
+  private apiUrl = "http://localhost:8080/api/v1";
+
+  userProfile = signal<LoginResponse|null>(null);
+
+  post<LoginRequest>(payload:LoginRequest): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.apiUrl}/users/login`,payload);
+  }
+}
--- elearn-client/src/app/components/login/login.ts
+++ elearn-client/src/app/components/login/login.ts
@@ -1,11 +1,28 @@
-import { Component } from "@angular/core";
-
+import { Component, inject } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { LoginService } from "./login.service";
 
 @Component({
   selector : "app-login",
-  imports : [],
+  imports : [ReactiveFormsModule],
   templateUrl : "login.html"
 })
 export class Login {
+  private fb = inject(FormBuilder);
+  private loginService =inject(LoginService);
 
+  form = this.fb.group({
+    usernameOrEmail : ['',[Validators.required]],
+    password : ['',[Validators.required,Validators.minLength(8)]],
+  })
+
+  submit() {
+    if (this.form.invalid) return;
+
+    this.loginService.post(this.form.value).subscribe({
+      next : (res) => this.loginService.userProfile.set(res),
+      error : (err) => console.log("Error:", err)
+    });
+
+  }
 }
