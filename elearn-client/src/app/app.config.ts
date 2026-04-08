2a9fcb0c63da3a70a009c2fe788a1d5a6025d9cb
Soufiane <jabersoufiane605@gmail.com>
2026-04-08 09:29:22 +0100

    tanstack query config

--- elearn-client/src/app/app.config.ts
+++ elearn-client/src/app/app.config.ts
@@ -1,11 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTanStackQuery, QueryClient } from '@tanstack/angular-query-experimental';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
+    provideTanStackQuery(new QueryClient()),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration(withEventReplay())
   ]
