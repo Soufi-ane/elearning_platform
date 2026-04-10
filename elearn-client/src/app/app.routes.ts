e9c0d31cbdef9acd1c7f19fe149c971540c0d70e
Soufiane <jabersoufiane605@gmail.com>
2026-04-10 10:39:36 +0100

    add authGaurd to for fix null error

--- elearn-client/src/app/app.routes.ts
+++ elearn-client/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Login } from './components/login/login';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Profile } from './components/profile/profile';
+import { authGuard } from './components/login/auth.guard';
 
 export const routes: Routes = [
   { path : '', component : Home },
   { path : 'login', component : Login },
+  { path : 'dashboard', component : Dashboard, canActivate : [authGuard] },
+  { path : 'dashboard/profile', component : Profile, canActivate : [authGuard] },
+  { path : '**', redirectTo : "/" },
 ];
