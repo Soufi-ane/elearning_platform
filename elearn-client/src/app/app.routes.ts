2004f968e0ee5f9beadd88b28598a18aa9943532
Soufiane <jabersoufiane605@gmail.com>
2026-04-15 18:55:47 +0100

    add link to calendar route

--- elearn-client/src/app/app.routes.ts
+++ elearn-client/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { Login } from './components/login/login';
 import { Dashboard } from './components/dashboard/dashboard';
 import { Profile } from './components/profile/profile';
 import { authGuard } from './components/login/auth.guard';
+import { Calendar } from './components/calendar/calendar';
 
 export const routes: Routes = [
   { path : '', component : Home },
   { path : 'login', component : Login },
   { path : 'dashboard', component : Dashboard, canActivate : [authGuard] },
   { path : 'dashboard/profile', component : Profile, canActivate : [authGuard] },
+  { path : 'dashboard/calendar', component : Calendar, canActivate : [authGuard] },
   { path : '**', redirectTo : "/" },
 ];
