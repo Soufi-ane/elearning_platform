0070bf00ecfe28ac508e12fbfe9f93783483f79d
Soufiane <jabersoufiane605@gmail.com>
2026-07-06 11:15:54 +0100

    list results by semester & user id

--- elearn-core/src/main/java/com/elearn/api/repository/ResultRepository.java
+++ elearn-core/src/main/java/com/elearn/api/repository/ResultRepository.java
@@ -1,8 +1,13 @@
 package com.elearn.api.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.elearn.api.entity.Result;
 
 @Repository
-public interface ResultRepository extends JpaRepository<Result, String> {}
+public interface ResultRepository extends JpaRepository<Result, String> {
+
+  public List<Result> findByElement_Module_SemesterAndStudent_Id(int semester, String studentId);
+
+}
--- elearn-core/src/main/java/com/elearn/api/service/ResultService.java
+++ elearn-core/src/main/java/com/elearn/api/service/ResultService.java
@@ -1,7 +1,9 @@
 package com.elearn.api.service;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.elearn.api.entity.Schemas.ResultResponse;
 import com.elearn.api.repository.ResultRepository;
 
 @Service 
@@ -11,6 +13,13 @@ public class ResultService {
   @Autowired
   public ResultService(ResultRepository resultRepository){
     this.resultRepository = resultRepository;
+  }
+
+  public List<ResultResponse> listBySemester(int semester, String userId){
+    return resultRepository.findByElement_Module_SemesterAndStudent_Id(semester, userId)
+      .stream()
+      .map(r -> new ResultResponse(r))
+      .toList();
   }
 
 }
