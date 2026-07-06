28945babf71389143a2178d0b925244ae6d16d2e
Soufiane <jabersoufiane605@gmail.com>
2026-07-06 11:16:16 +0100

    user results by semester

--- elearn-core/src/main/java/com/elearn/api/controller/ResultController.java
+++ elearn-core/src/main/java/com/elearn/api/controller/ResultController.java
@@ -1,8 +1,16 @@
 package com.elearn.api.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.elearn.api.entity.Schemas.ResultResponse;
+import com.elearn.api.entity.Role;
+import com.elearn.api.entity.User;
 import com.elearn.api.service.ResultService;
 
 @RestController
@@ -13,6 +21,17 @@ public class ResultController {
   @Autowired
   public ResultController(ResultService resultService){
     this.resultService = resultService;
+  }
+
+  @GetMapping("/{studentId}/{semester}")
+  public List<ResultResponse> listStudentResultsBySemester(
+      @PathVariable("studentId") String studentId,
+      @PathVariable("semester") int semester,
+      @AuthenticationPrincipal UserDetails userDetails
+    ){
+    User user = (User) userDetails;
+    boolean isNotStudent = user.getRole() != Role.STUDENT;
+    return resultService.listBySemester(semester, isNotStudent ? studentId : user.getId());
   }
 
 }
