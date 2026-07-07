70e5accee9bf7b71fb96c1bf40370d5bc6cc73f4
Soufiane <jabersoufiane605@gmail.com>
2026-07-07 20:03:10 +0100

    remove department from entities

--- elearn-core/src/main/java/com/elearn/api/repository/DepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.elearn.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.elearn.api.entity.Department;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, String> { }
