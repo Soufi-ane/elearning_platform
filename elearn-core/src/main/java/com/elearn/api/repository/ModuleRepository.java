eb1470817a7f3a96b4547fe2f6f62988e02e465d
Soufiane <jabersoufiane605@gmail.com>
2026-07-18 13:49:30 +0100

    add module repository

--- elearn-core/src/main/java/com/elearn/api/repository/ModuleRepository.java
+++ elearn-core/src/main/java/com/elearn/api/repository/ModuleRepository.java
@@ -1,8 +1,15 @@
 package com.elearn.api.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.elearn.api.entity.DepartmentName;
 import com.elearn.api.entity.Module;
 
 @Repository
-public interface ModuleRepository extends JpaRepository<Module, String> { }
+public interface ModuleRepository extends JpaRepository<Module, String> {
+
+    List<Module> findByDepartmentAndSemester(DepartmentName department, int semester);
+    List<Module> findDistinctByElementsTeacherId(String teacherId);
+
+}
