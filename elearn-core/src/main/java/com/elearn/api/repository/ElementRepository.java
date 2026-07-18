28f45fd2c1d31097abf650f7c062452a9e7ed725
Soufiane <jabersoufiane605@gmail.com>
2026-07-18 13:49:38 +0100

    add element repository

--- elearn-core/src/main/java/com/elearn/api/repository/ElementRepository.java
+++ elearn-core/src/main/java/com/elearn/api/repository/ElementRepository.java
@@ -1,8 +1,12 @@
 package com.elearn.api.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.elearn.api.entity.Element;
 
 @Repository
-public interface ElementRepository extends JpaRepository<Element, String> { }
+public interface ElementRepository extends JpaRepository<Element, String> {
+    List<Element> findByModuleId(String moduleId);
+    List<Element> findByModuleIdAndTeacherId(String moduleId, String teacherId);
+}
