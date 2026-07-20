dbe4bf38eb28025b7263ccaa03b7a1d9989a71c7
Soufiane <jabersoufiane605@gmail.com>
2026-07-20 18:55:24 +0100

    fix routes

--- elearn-client/src/app/app.routes.ts
+++ elearn-client/src/app/app.routes.ts
@@ -3,15 +3,16 @@ import { Home } from './home/home';
 import { Login } from './components/login/login';
 import { Dashboard } from './components/dashboard/dashboard';
 import { Profile } from './components/profile/profile';
-import { authGuard } from './components/login/auth.guard';
 import { MainDashboard } from './components/dashboard/main/main';
 import { Calendar } from './components/dashboard/calendar/calendar';
-import { Attendance } from './components/dashboard/attendance/attendance';
-import { AbsenceView } from './components/dashboard/absence/absence';
 import { Grades } from './components/dashboard/grades/grades';
 import { Requests } from './components/dashboard/requests/requests';
 import { ModuleList } from './components/dashboard/modules/module-list';
 import { ElementDetail } from './components/dashboard/modules/element-detail';
+import { roleGuard } from './components/login/auth.guard';
+import { Attendance } from './components/dashboard/attendance/attendance';
+import { AbsenceView } from './components/dashboard/attendance/absence-view';
+import { AdminAbsence } from './components/dashboard/attendance/admin-absence';
 
 export const routes: Routes = [
   { path : '', component : Home },
@@ -19,12 +20,17 @@ export const routes: Routes = [
   {
     path : 'dashboard',
     component : Dashboard,
-    canActivate : [authGuard],
+    canActivate : [roleGuard()],
     children : [
       { path : '', component : MainDashboard },
       { path : 'profile', component : Profile },
       { path : 'calendar', component : Calendar },
       { path : 'attendance', component : Attendance },
+      {
+        path : 'attendance/manage',
+        component : AdminAbsence,
+        canActivate : [roleGuard(['ADMIN', 'TEACHER'])]
+      },
       { path : 'attendance/:id', component : AbsenceView },
       { path : 'grades', component : Grades },
       { path : 'requests', component : Requests },
