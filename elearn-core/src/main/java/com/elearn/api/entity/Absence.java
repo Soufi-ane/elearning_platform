def6d924aa8ad2951b5ba4529bc2022065ad5676
Soufiane <jabersoufiane605@gmail.com>
2026-07-20 18:56:50 +0100

    fix absence entity

--- elearn-core/src/main/java/com/elearn/api/entity/Absence.java
+++ elearn-core/src/main/java/com/elearn/api/entity/Absence.java
@@ -2,16 +2,25 @@ package com.elearn.api.entity;
 
 import java.time.LocalDateTime;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Data;
 
 @Entity
+@Table(
+  uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"student_id", "element_id", "date_time"})
+  }
+)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -33,6 +42,8 @@ public class Absence {
   private String id;
 
   private LocalDateTime dateTime;
+
+  @Enumerated(EnumType.STRING)
   private AbsenceType type;
   private boolean isJustified;
 
