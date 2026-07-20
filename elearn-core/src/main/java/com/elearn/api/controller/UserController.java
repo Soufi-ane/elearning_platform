49d815b4f213f1587cd9c53712134802b6d8d943
Soufiane <jabersoufiane605@gmail.com>
2026-07-20 18:56:34 +0100

    get users by role

--- elearn-core/src/main/java/com/elearn/api/controller/UserController.java
+++ elearn-core/src/main/java/com/elearn/api/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.elearn.api.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.elearn.api.entity.User;
+import com.elearn.api.entity.Role;
 import com.elearn.api.entity.Schemas.LoginRequest;
 import com.elearn.api.entity.Schemas.RegisterRequest;
 import com.elearn.api.entity.Schemas.UserResponse;
@@ -45,6 +48,25 @@ public class UserController {
   @PostMapping("/logout")
   public ResponseEntity<?> logout(HttpServletResponse response){
     return userService.logout(response);
+  }
+
+  @GetMapping
+  public ResponseEntity<?> getUsers(
+    @RequestParam(required = false) Role role,
+    @AuthenticationPrincipal UserDetails userDetails) 
+  {
+
+    if (userDetails == null) {
+      return ResponseEntity.status(401).build();
+    }
+
+    User currentUser = (User) userDetails;
+
+    if (currentUser.getRole() != Role.ADMIN && currentUser.getRole() != Role.TEACHER) {
+      return ResponseEntity.status(403).body("Access denied");
+    }
+    List<UserResponse> users = userService.getUsersByRole(role);
+    return ResponseEntity.ok(users);
   }
 
 }
