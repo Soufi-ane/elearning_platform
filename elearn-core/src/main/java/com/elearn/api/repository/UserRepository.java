ac233d13160508063e6732fb12cfd6a513b86099
Soufiane <jabersoufiane605@gmail.com>
2026-07-20 18:57:08 +0100

    file users by role

--- elearn-core/src/main/java/com/elearn/api/repository/UserRepository.java
+++ elearn-core/src/main/java/com/elearn/api/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.elearn.api.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.elearn.api.entity.Role;
 import com.elearn.api.entity.User;
 
 @Repository
@@ -11,5 +13,7 @@ public interface UserRepository extends JpaRepository<User, String> {
   public Optional<User> findByUsernameOrEmail(String username, String email);
 
   public Optional<User> findByEmail(String email);
+
+  List<User> findByRole(Role role);
   
 }
