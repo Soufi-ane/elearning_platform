58ed0c3485186e5b1a3f26049d999e44a5a9e3eb
Soufiane <jabersoufiane605@gmail.com>
2026-07-23 10:21:02 +0100

    fix dashborad

--- elearn-client/src/app/components/dashboard/dashboard.ts
+++ elearn-client/src/app/components/dashboard/dashboard.ts
@@ -1,13 +1,18 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { LoginService } from '../login/login.service';
+import { Role } from '../../models/models';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './dashboard.html',
 })
 export class Dashboard {
   private loginService = inject(LoginService);
   user = this.loginService.userProfile()!;
+
+  get isStudent(): boolean {
+    return this.user?.role === Role.STUDENT || (this.user?.role as unknown as string) === 'STUDENT';
+  }
 }
